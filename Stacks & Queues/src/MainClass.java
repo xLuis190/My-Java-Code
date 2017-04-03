735def508b38bb34d3d11db20504cfc6be40b009
Luis Felipe <xLuis190@hotmail.com>
2017-04-03 00:12:39 -0400

    Check which stack is faster

--- Stacks & Queues/src/MainClass.java
+++ Stacks & Queues/src/MainClass.java
@@ -5,28 +5,32 @@ import java.util.*;
  * Created by XLuis on 4/1/2017.
  */
 public class MainClass {
+
     public static void main(String[] args){
+        MainClass mainclass = new MainClass();
+        mainclass.checkForMyStack();
+        mainclass.checkForRegularStack();
+    }
+    public void checkForRegularStack(){
+        long startTime = System.currentTimeMillis();
+        //Stacks
+        Stack myStack = new Stack();
+        for(int x = 0;x<=100000000;x++){
+            myStack.push("Hello World");
+        }
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("This is the regular Stack "+totalTime);
+    }
+    public void checkForMyStack(){
+        long startTime = System.currentTimeMillis();
         //Stacks
         Stacks myStack = new Stacks();
-        System.out.println(myStack.StackEmpty());
-        myStack.Push("Hello");
-        myStack.Push("Booo");
-        myStack.Pop();
-        myStack.printElements();
-        System.out.println(myStack.StackEmpty());
-
-
-
-        //Queues
-        Queues myQueue = new Queues();
-        myQueue.EnQueue("Hello World");
-        myQueue.EnQueue("Hello World1");
-        myQueue.EnQueue("Hello World2");
-        myQueue.EnQueue("Hello World3");
-        myQueue.DeQueue();
-        myQueue.DeQueue();
-        myQueue.DeQueue();
-        myQueue.ShowElements();
-
+        for(int x = 0;x<=100000000;x++){
+            myStack.Push("Hello World"+x);
+        }
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("This is my awesome stack "+totalTime);
     }
 }
--- Stacks & Queues/src/Stacks.java
+++ Stacks & Queues/src/Stacks.java
@@ -3,29 +3,23 @@ import java.util.ArrayList;
 
 public class Stacks {
     private ArrayList stack;
-    int top;
+
     public Stacks(){
         stack = new ArrayList();
     }
     public boolean StackEmpty(){
-        if(top == 0){
-            return true;
-        }
-        else {
-            return false;
-        }
+        if(stack.size() == 0)return true;
+        else return false;
     }
     public void Push(Object element){
         stack.add(element);
-        top +=1;
 
     }
-    public void Pop(){
+    public void Pull(){
          stack.remove(stack.size() -1);
-        top =-1;
     }
     public int getSize(){
-        return top;
+        return stack.size();
     }
     public void printElements(){
         System.out.println(stack);
